c4ca65bc07a963ae21835ce96d5339e5e34ca7b1
Shyam <shyam3145@gmail.com>
2026-05-29 13:08:14 +0530

    fix: new project ID for thampi3145

--- app.config.js
+++ app.config.js
@@ -26,9 +26,9 @@ module.exports = {
     plugins: ["expo-image-picker"],
     extra: {
       eas: {
-        projectId: "49814a72-cbf7-447e-92c9-098c3fc8ee38"
+      projectId: "f1d5d80a-1eaa-4048-956f-0003f193e8f3" 
       }
     },
-    owner: "shyam3145"
+    owner: "thampi3145"
   }
 };
